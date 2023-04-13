76235633570e89683fc5e0fa5ec88306af2c8d2b
barissamur <barissamurr@gmail.com>
2023-04-13 10:34:29 +0300

    container

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Todo List</h1>
+      <main className='container'>
+        <h1 className='mt-4'>Todo List</h1>
         <TodoList />
       </main>
     </>
